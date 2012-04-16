9c635c0963a0241d58424e56213a3b2a2525b4bb
guillemNaN <guillemNaN@localhost>
2012-04-16 10:02:56 +0200

    [IMP] PEP8 formatting and fields and function reodered

--- product.py
+++ product.py
@@ -1,9 +1,12 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this :repository contains the full copyright notices and license terms.
+
+from decimal import Decimal
+
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pyson import Eval, Bool
 from trytond.pool import Pool
-from decimal import Decimal
+
 
 class ProductKitLine(ModelSQL, ModelView):
     '''Product Kit'''
@@ -12,6 +15,8 @@ class ProductKitLine(ModelSQL, ModelView):
 
     parent = fields.Many2One('product.product', 'Parent Product', required=True,
             ondelete='CASCADE')
+    sequence = fields.Integer('sequence')
+
     product = fields.Many2One('product.product', 'Product', required=True,
 #            domain=[
 #                ('id', '!=', Eval('parent_parent')),
@@ -21,10 +26,10 @@ class ProductKitLine(ModelSQL, ModelView):
         fields.Many2One('product.uom.category', 'Product Uom Category',
             on_change_with=['product']),
         'get_product_uom_category')
-    
-    sequence = fields.Integer('sequence')
+
     quantity = fields.Float('Quantity', digits=(16, Eval('unit_digits', 2)),
             required=True, depends=['unit_digits'])
+
     unit = fields.Many2One('product.uom', 'Unit', required=True,
             domain=[
                 ('category', '=', Eval('product_uom_category')),
@@ -37,18 +42,28 @@ class ProductKitLine(ModelSQL, ModelView):
     unit_digits = fields.Function(fields.Integer('Unit Digits',
             on_change_with=['unit']), 'get_unit_digits')
 
-    def on_change_with_product_uom_category(self, vals):
-        product_obj = Pool().get('product.product')
-        if vals.get('product'):
-            product = product_obj.browse(vals['product'])
-            return product.default_uom_category.id
-
+    # product.kit.line
     def get_product_uom_category(self, ids, name):
         categories = {}
         for line in self.browse(ids):
             categories[line.id] = line.product.default_uom_category.id
         return categories
 
+    # product.kit.line
+    def on_change_with_product_uom_category(self, vals):
+        product_obj = Pool().get('product.product')
+        if vals.get('product'):
+            product = product_obj.browse(vals['product'])
+            return product.default_uom_category.id
+
+    # product.kit.line
+    def on_change_with_unit(self, vals):
+        product_obj = Pool().get('product.product')
+        if vals.get('product'):
+            product = product_obj.browse(vals['product'])
+            return product.default_uom.id
+
+    # product.kit.line
     def get_unit_digits(self, ids, name):
         res = {}
         for line in self.browse(ids):
@@ -58,12 +73,7 @@ class ProductKitLine(ModelSQL, ModelView):
                 res[line.id] = 2
         return res
 
-    def on_change_with_unit(self, vals):
-        product_obj = Pool().get('product.product')
-        if vals.get('product'):
-            product = product_obj.browse(vals['product'])
-            return product.default_uom.id
-
+    # product.kit.line
     def on_change_with_unit_digits(self, vals):
         uom_obj = Pool().get('product.uom')
         if vals.get('unit'):
@@ -71,6 +81,7 @@ class ProductKitLine(ModelSQL, ModelView):
             return uom.digits
         return 2
 
+    # product.kit.line
     def __init__(self):
         super(ProductKitLine, self).__init__()
         self._order.insert(0, ('sequence', 'ASC'))
@@ -82,6 +93,7 @@ class ProductKitLine(ModelSQL, ModelView):
             'recursive_kits': 'You can not create recursive kits!',
         })
 
+    # product.kit.line
     def check_recursion(self, ids, parent='parent'):
 
         def check_recursion_product(products, all_products):
@@ -89,13 +101,13 @@ class ProductKitLine(ModelSQL, ModelView):
 
             if not products:
                 return True
-            
-            new_products =[]
+
+            new_products = []
             for product in product_obj.browse(products):
                 if product.kit and product.id in all_products:
                     return False
                 elif not product.kit:
-                    continue                    
+                    continue
                 new_products.append(product.id)
 
             if new_products:
@@ -104,34 +116,30 @@ class ProductKitLine(ModelSQL, ModelView):
 
             return True
 
-        products=[]
+        products = []
         for kit_line in self.browse(ids):
             if kit_line.product.kit:
-                products += [ kit_line.product.id]
+                products += [kit_line.product.id]
 
-        return check_recursion_product( products, products )
+        return check_recursion_product(products, products)
 
 ProductKitLine()
 
-STATES = {
-    'readonly': ~Eval('active', True),
-}
-DEPENDS = ['active']
 
 class Product(ModelSQL, ModelView):
     _name = "product.product"
 
     kit = fields.Boolean('Kit?')
-    kit_lines = fields.One2Many('product.kit.line', 'parent', 
+    kit_lines = fields.One2Many('product.kit.line', 'parent',
             'Components', states={
-                    'readonly': Bool(~Eval('kit')),
-                    }, depends=['kit'])
+                'readonly': Bool(~Eval('kit')),
+            }, depends=['kit'])
     kit_fixed_list_price = fields.Boolean('Fixed List Price', help='Mark this '
             'field if the list price of the kit should be fixed. Do not mark '
             'it if the price should be calculated from the sum of the prices '
             'of the products in the pack.'),
 
-
+    # product.product
     def explode_kit(self, product_id, quantity, unit, depth=1):
         """
         Walks through the Kit tree in depth-first order and returns
@@ -140,7 +148,8 @@ class Product(ModelSQL, ModelView):
         uom_obj = Pool().get('product.uom')
         result = []
         for line in self.browse(product_id).kit_lines:
-            qty = quantity * uom_obj.compute_qty(line.unit, line.quantity, unit)
+            qty = quantity * uom_obj.compute_qty(line.unit, line.quantity,
+                    unit)
             result.append({
                     'product_id': line.product.id,
                     'quantity': qty,
@@ -149,7 +158,7 @@ class Product(ModelSQL, ModelView):
                     'depth': depth,
                     })
             result += self.explode_kit(line.product.id, quantity,
-                    line.unit, depth+1)
+                    line.unit, depth + 1)
         return result
 
 Product()
