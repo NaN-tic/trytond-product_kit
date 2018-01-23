5c1d9b33281bd6bccc221a30e64e30800fe07b1d
C?dric Krier <ced@b2ck.com>
2018-01-23 18:07:02 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-#This file is part product_kit module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part product_kit module for Tryton.  The COPYRIGHT file at the
+# top level of this repository contains the full copyright notices and license
+# terms.
 from trytond.pool import Pool
-from .product import *
+from . import product
 
 
 def register():
     Pool.register(
-        ProductKitLine,
-        Product,
+        product.ProductKitLine,
+        product.Product,
         module='product_kit', type_='model')
